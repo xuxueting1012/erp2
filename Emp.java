3de55c6d6faee4bbbf8e13da9dd80ea3681b46ae
xxt <xxt@163.com>
2020-09-05 09:29:49 +0800

    ddd

--- Emp.java
+++ Emp.java
@@ -2,5 +2,8 @@ public class Hello{
 	 public static void main(string args[]) {
 		 System.out.println("Hello GIT");
 		  System.out.println("修改了bug");
+		  System.out.println("伤心1");
+		  System.out.println("伤心2");
+		  System.out.println("伤心3");
 	 }
 }
