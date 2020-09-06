64d68b652bea8a499ef396cfdebccd777e7a9a3b
xxt <xxt@163.com>
2020-09-06 08:28:53 +0800

    change Emp.java

--- Emp.java
+++ Emp.java
@@ -1,9 +1,10 @@
-public class Hello{
+public class Emp{
 	 public static void main(string args[]) {
 		 System.out.println("Hello GIT");
 		  System.out.println("修改了bug");
 		  System.out.println("伤心1");
 		  System.out.println("伤心2");
 		  System.out.println("伤心3");
+		    System.out.println("dev");
 	 }
 }
